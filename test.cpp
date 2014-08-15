92531460cbba861a2f8923254479f79f858c07a9
anthropomorphic <code@mikefdorst.com>
2014-08-14 21:24:58 -0700

    Add test for sorted_tree<int>::find()

--- test.cpp
+++ test.cpp
@@ -18,6 +18,31 @@ SCENARIO("nstd::sorted_tree", "[sorted_tree]") {
         
     }
     
+    GIVEN("a sorted_tree<int> with some values in proper sort order") {
+        
+        sorted_tree<int> tree;
+        
+        tree.root_node = make_shared<tree_node<int>>(64);
+        tree.root_node->lnode = make_shared<tree_node<int>>(32);
+        tree.root_node->lnode->lnode = make_shared<tree_node<int>>(16);
+        tree.root_node->lnode->rnode = make_shared<tree_node<int>>(48);
+        tree.root_node->rnode = make_shared<tree_node<int>>(96);
+        tree.root_node->rnode->lnode = make_shared<tree_node<int>>(80);
+        tree.root_node->rnode->rnode = make_shared<tree_node<int>>(112);
+        
+        WHEN("find is called on those values") {
+            
+            REQUIRE(tree.find(16)  == tree.root_node->lnode->lnode);
+            REQUIRE(tree.find(32)  == tree.root_node->lnode);
+            REQUIRE(tree.find(48)  == tree.root_node->lnode->rnode);
+            REQUIRE(tree.find(64)  == tree.root_node);
+            REQUIRE(tree.find(80)  == tree.root_node->rnode->lnode);
+            REQUIRE(tree.find(96)  == tree.root_node->rnode);
+            REQUIRE(tree.find(112) == tree.root_node->rnode->rnode);
+            
+        }
+        
+    }
     
 }
 
