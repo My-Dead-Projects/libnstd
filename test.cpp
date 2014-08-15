da6f7c9dd2ebc823bf69fc1ddb876b0a7a9f0f3f
anthropomorphic <code@mikefdorst.com>
2014-08-14 17:59:29 -0700

    Add tree_node.hpp

--- test.cpp
+++ test.cpp
@@ -2,4 +2,5 @@
 #define CATCH_CONFIG_MAIN
 #include "Catch/single_include/catch.hpp"
 
+#include "tree_node.hpp"
 
---
+++ tree_node.hpp
@@ -0,0 +1,24 @@
+#ifndef libnstd_tree_node_hpp
+#define libnstd_tree_node_hpp
+
+#include <memory>
+
+namespace nstd {
+
+template <class T>
+class tree_node {
+public:
+    T value;
+    std::shared_ptr<tree_node<T>> lnode;
+    std::shared_ptr<tree_node<T>> rnode;
+    tree_node(const T&);
+};
+
+}
+
+template <class T>
+nstd::tree_node<T>::tree_node(const T& v) : value(v), lnode(nullptr), rnode(nullptr) {
+    
+}
+
+#endif
