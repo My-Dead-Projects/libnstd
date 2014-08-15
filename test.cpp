a1d322c8a4f1d9cc224b0dc98d3f0831c190e33f
anthropomorphic <code@mikefdorst.com>
2014-08-14 20:12:19 -0700

    Require that root_node is empty on construction

--- test.cpp
+++ test.cpp
@@ -3,3 +3,13 @@
 
 #include "sorted_tree.hpp"
 
+SCENARIO("nstd::sorted_tree", "[sorted_tree]") {
+    
+    GIVEN("a nstd::sorted_tree<int>") {
+        
+        nstd::sorted_tree<int> tree;
+        
+        REQUIRE(!tree.root_node);
+    }
+    
+}
